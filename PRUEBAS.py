87de5aff46424446e01429b7511f8d719127da01
kevinsoler313 <kevinsoler313@gmail.com>
2025-03-12 20:15:13 -0500

    prueba pc GAMER

---
+++ PRUEBAS.py
@@ -0,0 +1,9 @@
+def holi_mundi(variable):
+    if variable == "0":
+        print ("EL NUMERO ES CERO")
+    else:
+        print ("ES ALGUN OTRO NUMERO")
+
+num = input("DIGITE EL NUMERO: ")
+
+holi_mundi(num)
